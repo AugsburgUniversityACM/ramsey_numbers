30494de339f764f966a98b59aea5575742b6d4d5
Jeron Aldaron Lau <jeronaldaron@gmail.com>
2019-03-09 17:45:06 -0600

    Wrote the part of the algorithm that generates different complete graphs and cycles through them.

--- rs_calcg/src/brute_force.rs
+++ rs_calcg/src/brute_force.rs
@@ -1,4 +1,60 @@
-/// Calculate the ramsey numbers in a brute-force loop.
+use crate::KGraphRBE;
+use crate::kgraphrbe::MAX_KGRAPHRBE_SIZE;
+
+/// Find the 2 biggest cliques in a KGraphRBE.
+fn cliques(n_vertices: usize, kgraphrbe: KGraphRBE) -> (usize, usize) {
+    // Look for first edge's (AB) color (treat this as RED).
+    let edge = kgraphrbe.get(0);
+    let mut r = 1;
+    let mut b = 0;
+
+    // Start at 2 (Vertex C)
+    for i in 2..n_vertices {
+        kgraphrbe.get(0);
+    }
+
+    // Return smaller clique first, then the bigger clique.
+    if r > b {
+        (b, r) // small, big
+    } else {
+        (r, b) // small, big
+    }
+}
+
+/// Generate differently colored graphs to find cliques in.
+pub fn calculate(n_vertices: usize, n_edges: usize) -> (usize, usize) {
+    println!("Vertices: {}, Edges: {}", n_vertices, n_edges);
+
+    let mut kgraphrbe = KGraphRBE::new(n_edges);
+    let mut minb = std::usize::MAX; // Big Clique
+    let mut minr = std::usize::MAX; // Small Clique
+
+    loop {
+        if kgraphrbe.increment() {
+            break;
+        }
+        let (r, b) = cliques(n_vertices, kgraphrbe.clone());
+        minr = minr.min(r);
+        minb = minb.min(b);
+    }
+
+    (minr, minb)
+}
+
+/// Calculate the ramsey numbers R(x, x) in a brute-force loop.
 pub fn brute_force() {
-    
+    // Search through different sizes of graphs and detect cliques.
+    let mut n_vertices = 2;
+
+    loop {
+        let n_edges = (n_vertices * (n_vertices - 1)) >> 1;
+
+        if n_edges > MAX_KGRAPHRBE_SIZE {
+            break;
+        }
+
+        println!("R{:?} =? {}", calculate(n_vertices, n_edges), n_vertices);
+
+        n_vertices += 1;
+    }
 }
--- rs_calcg/src/kgraphrbe.rs
+++ rs_calcg/src/kgraphrbe.rs
@@ -1,9 +1,11 @@
+/// The maximum number of edges.
 pub const MAX_KGRAPHRBE_SIZE: usize = 64 * 32;
 
 /// A complete graph colored with red and blue edges.
+#[derive(Clone)]
 pub struct KGraphRBE {
-    // Number of vertices in the graph.
-    size: usize,
+    // Number of edges in the graph.
+    edges: usize,
     // Bitstring of edge colors.
     colors: [u64; 32],
 }
@@ -11,9 +13,9 @@ pub struct KGraphRBE {
 impl KGraphRBE {
     /// Create a new Red/Blue complete graph.
     #[inline(always)]
-    pub fn new(size: usize) -> Self {
+    pub fn new(edges: usize) -> Self {
         KGraphRBE {
-            size,
+            edges,
             colors: [0; 32],
         }
     }
@@ -21,33 +23,62 @@ impl KGraphRBE {
     /// Set the color for an edge to blue (0).
     #[inline(always)]
     pub fn setb(&mut self, index: usize) {
+        debug_assert!(index < self.edges);
+
         let i = index / 64;
         let j = index % 64;
 
-        print!("BFrom {:b}", self.colors[i]);
+//        print!("BFrom {:b}", self.colors[i]);
         self.colors[i] &= !(1 << j);
-        println!(" to {:b}", self.colors[i]);
+//        println!(" to {:b}", self.colors[i]);
     }
 
     /// Set the color for an edge to red (1).
     #[inline(always)]
     pub fn setr(&mut self, index: usize) {
+        debug_assert!(index < self.edges);
+
         let i = index / 64;
         let j = index % 64;
 
-        print!("RFrom {:b}", self.colors[i]);
+//        print!("RFrom {:b}", self.colors[i]);
         self.colors[i] |= 1 << j;
-        println!(" to {:b}", self.colors[i]);
+//        println!(" to {:b}", self.colors[i]);
     }
 
     /// Get the color for an edge.  1 for red, 0 for blue.
     #[inline(always)]
-    pub fn get(&mut self, index: usize) -> bool {
+    pub fn get(&self, index: usize) -> bool {
+        debug_assert!(index < self.edges);
+
         let i = index / 64;
         let j = index % 64;
 
-        println!("Get {:b}", self.colors[i]);
+//        println!("Get {:b}", self.colors[i]);
         (self.colors[i] & (1 << j)) != 0
+    }
+
+    #[inline(always)]
+    pub fn n_edges(&self) -> usize {
+        self.edges
+    }
+
+    #[inline(always)]
+    pub fn increment(&mut self) -> bool {
+        let mut digit = 0;
+
+        loop {
+            if self.get(digit) {
+                self.setb(digit);
+                digit += 1;
+            } else {
+                self.setr(digit);
+                break;
+            }
+        }
+
+        // TODO: should be >= ?
+        digit > (self.edges >> 1)
     }
 }
 
