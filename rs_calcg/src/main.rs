4b5ed8124ea3cdff09a139ca12b9270c7c50b922
Jeron Aldaron Lau <jeronaldaron@gmail.com>
2019-03-30 14:30:39 -0500

    Verified algorithm seems to work now!

--- rs_calcg/src/main.rs
+++ rs_calcg/src/main.rs
@@ -1,3 +1,8 @@
+//! Calculates fast for
+//! * all R(1, _)
+//! * no larger than R(2, 6)
+//! * no larger R(3, 3)
+
 mod brute_force;
 mod cliques;
 mod graphe;
@@ -13,7 +18,7 @@ pub use math::{is_triangular, triangle_num, triangle_root};
 pub use simd::{print_enabled, simd_and, simd_eq, simd_is_zero};
 
 const R: usize = 3;
-const S: usize = 3;
+const S: usize = 4;
 
 fn main() {
     // Print whether or not we have SIMD
